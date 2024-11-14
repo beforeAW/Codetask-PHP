8b1680a6955e7ec6609b41b4742f73f6d68d0b37
Albin Wrebo <albin.wrebo@outlook.com>
2024-11-14 19:01:03 +0100

    done

--- src/index.php
+++ src/index.php
@@ -10,16 +10,59 @@
 <body>
 
 <div class="profile-card">
-    <h1><?php 
-    echo "hi world!";?><!-- PHP: Place the user name here --></h1>
-    <p class="greeting"><!-- PHP: Display the personalized greeting here --></p>
-    <p>Age: <!-- PHP: Display the user age here --></p>
-    <p>City: <!-- PHP: Display the user city here --></p>
-    <p class="status"><!-- PHP: Display status based on age (Adult or Minor) --></p>
+    <?php
+        #Variables & Array:
+        $name = "Dave";
+        $age = 18;
+        $city = "Stockholm";
+        $hobbies = [
+            "Videogames",
+            "Movies",
+            "Boardgames",
+            "Music",
+            "Anime",
+        ];
+    ?>
+    <h1>
+        <?php 
+            echo $name;
+        ?><!-- PHP: Place the user name here -->
+    </h1>
+    <p class="greeting">
+        <?php
+            $greeting = "Greetings " . $name . "!";
+            echo $greeting;
+        ?><!-- PHP: Display the personalized greeting here --></p>
+    <p>Age: 
+        <?php
+            echo $age;
+        ?><!-- PHP: Display the user age here -->
+    </p>
+    <p>City: 
+        <?php
+            echo $city;
+        ?><!-- PHP: Display the user city here -->
+    </p>
+    <p class="status">
+        <?php
+            if ($age >= 18){
+                $adult = " " . $name . " is an adult.";
+                echo $adult;
+            }
+            else{
+                $minor = " " . $name . " is a minor.";
+                echo $minor;
+            }
+        ?><!-- PHP: Display status based on age (Adult or Minor) --></p>
 
     <h2>Hobbies</h2>
     <ul>
-        <!-- PHP: Loop through hobbies and display each hobby as a list item -->
+        <?php
+            foreach ($hobbies as $hobby){
+                $hobbyList = "<li> $hobby </li>";
+                echo $hobbyList;
+            }
+        ?><!-- PHP: Loop through hobbies and display each hobby as a list item -->
     </ul>
 </div>
 
